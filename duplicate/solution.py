bfa27096754697fbe4c19da28a798a489d5a74eb
WhiteRose404 <eddoughri.youssef@gmail.com>
2023-09-19 16:48:37 +0100

    duplicate: added some random cases

--- duplicate/solution.py
+++ duplicate/solution.py
@@ -1,9 +1,37 @@
 
 class Solution:
+
+    # invalid solution: it uses O(n) space
+    def sol(self, nums) -> int:
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # print(nums)
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num;
+            seen.add(num)
+        return -1;
+
     def duplicate(self, nums) -> int:
         """
         :type nums: List[int]
         :rtype: int
         """
-        pass
+        # s = sum(nums);
+        # m = max(nums);
+        # n = len(nums);
+        # print("arr: ", nums, "sum: ", s, "max: ", m, "len: ", n)
+        # for k in nums:
+            # sum_k = k*(k-1)//2;
+            # sum_k_m = max(k*(m-k-1), 0);
+            # sum_k_n = k*(n-m+1);
+            # sum_k_k = ((m-k)*(m-k-1))//2;
+            # print("k: ", k, "itr: ", sum_k, sum_k_m, sum_k_n, sum_k_k)
+            # itr = sum_k + sum_k_m + sum_k_n + sum_k_k;
+            # if(s == itr): return k;
+            # print(k, itr, s)
+        return -1;
 
--- duplicate/testing.py
+++ duplicate/testing.py
@@ -1,16 +1,57 @@
 import unittest
+import random
 
 from solution import Solution
 
 class Testing(unittest.TestCase):
-    def test_one(self):
-        self.assertEqual(Solution().duplicate([1,2,3,1]), 1);
-    def test_two(self):
-        self.assertEqual(Solution().duplicate([1,2,3,2]), 2);
-    def test_three(self):
-        self.assertEqual(Solution().duplicate([1,1,1,3,3,4,3,2,4,2]), 1);
-    def test_four(self):
-        self.assertEqual(Solution().duplicate([11,2,13,4,5,6,7,8,9,10,9,3,12,13,14]), 13);
+    def generatArray(self, n):
+        k = random.randint(1, n - 1);
+        t = random.randint(1, n - 1);
+        sequence = [i for i in range(1, n - t)];
+        arr = [];
+        while(len(sequence) > 0):
+            arr.append(sequence.pop(random.randint(0, len(sequence) - 1)));
+        for i in range(t):
+            length = max(1, len(arr));
+            # insert k at random position
+            arr.insert(random.randint(0, length - 1), k);
+        return arr;
+
+
+    # simple tests
+    def test1(self):
+        self.assertEqual(Solution().duplicate([1, 1]), 1);
+    def test2(self):
+        self.assertEqual(Solution().duplicate([1, 2, 3, 4, 1]), 1);
+    def test3(self):
+        self.assertEqual(Solution().duplicate([1, 2, 3, 4, 5, 5]), 5);
+
+    # random cases
+    def test14(self):
+        test_arr = self.generatArray(7);
+        self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    def test12(self):
+        test_arr = self.generatArray(5);
+        self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    def test20(self):
+        test_arr = self.generatArray(10);
+        self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    def test4(self):
+        test_arr = self.generatArray(80);
+        self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    # def test5(self):
+    #     test_arr = self.generatArray(5000);
+    #     self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    # def test6(self):
+    #     test_arr = self.generatArray(2000);
+    #     self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    # def test7(self):
+    #     test_arr = self.generatArray(300);
+    #     self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+    # def test8(self):
+    #     test_arr = self.generatArray(1000);
+    #     self.assertEqual(Solution().duplicate(test_arr), Solution().sol(test_arr));
+
 
 if __name__ == '__main__':
     unittest.main()
